0ad958f4b532a0d5460140769997cbd012bcd49e
PrAyAg9 <114804510+PrAyAg9@users.noreply.github.com>
2024-09-16 21:41:28 +0530

    Add files via upload

---
+++ Recursion/1_Introduction.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+using namespace std;
+// What is recursion - Function which call itself is the definition which everyone keeps blabbering...
+
+// Recursion is all about finding Recurrence Relation and base case 
+// Studying Recrrence Relation in deep could take time === SO Give it .....
+
+// But for normal functions just try to see a recurence relation and base case where function should stop. like in factorial or fibbonaci.
+int factorial(int n){
+    if(n==0 || n==1){
+        return 1;
+    }
+    return n*factorial(n-1);
+}
+int main(){
+    cout<< factorial(5);
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    // I have to do it with consistency today I am here with very little knowledge of Devlopement, DSA and CSE Core topics with no knowledge or
+    // real life skills which would be helpful for me, I am kinda depressed but yup have to make it big or just stand on my legs let it be my 
+    // first goal for now. Let's see how it turns out.
+
+    // Let's go as everyones say consistency matters doing something good consistently with your whole heart and god blessingsw
+    return 0;
+}
