e9da6c187f046d31f72def73c8870e2fbd98a844
PrAyAg9 <114804510+PrAyAg9@users.noreply.github.com>
2024-09-21 21:41:24 +0530

    Create 121_BestTimeToBuyandSellStock.cpp

---
+++ Leet Code Soltutions/121_BestTimeToBuyandSellStock.cpp
@@ -0,0 +1,21 @@
+// Best Solution in cpp -- Beats 100 % of Users.
+
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int min = INT_MAX;
+        int t_profit = 0;
+        int profit = 0;
+        
+        for(int i = 0; i < prices.size(); i++){
+            if(prices[i] < min){
+                min = prices[i];
+            }
+            profit = prices[i] - min;
+            if(t_profit < profit){
+                t_profit = profit;
+            }
+        }
+        return t_profit;
+    }
+};
